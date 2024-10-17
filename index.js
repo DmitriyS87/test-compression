85d827a2eb7489af8de740a6d7f34b05f349f667
Dmitriy Schukin <schukindmitriy@gmail.com>
2024-10-17 16:24:54 +0200

    added serialize function with base64, mocha chai tests

--- index.js
+++ index.js
@@ -3,14 +3,52 @@
 // max 1000
 // target compression ~50%
 // только ASCII символы
-/* 
-Примеры тестов: простейшие короткие, 
-случайные - 50 чисел, 100 чисел, 500 чисел, 1000 чисел, 
-граничные - все числа 1 знака, 
-все числа из 2х знаков, 
-все числа из 3х знаков, 
-каждого числа по 3 - всего чисел 900. */
 
-const serialize = () => {};
+import { Buffer } from "node:buffer";
 
-function deserialize() {}
+export const serialize = (numbers) => {
+  if (numbers.length <= 3) {
+    return numbers.join(",");
+  }
+
+  let bitString = "";
+
+  numbers.forEach((num) => {
+    bitString += Number(num).toString(2).padStart(9, "0");
+  });
+
+  let byteArray = [];
+  for (let i = 0; i < bitString.length; i += 8) {
+    let byte = bitString.slice(i, i + 8);
+    byteArray.push(parseInt(byte.padEnd(8, "0"), 2));
+  }
+
+  return Buffer.from(byteArray).toString("base64");
+};
+
+export const deserialize = (bytes) => {
+  if (bytes.length === 1) {
+    return [Number(bytes)];
+  }
+
+  if (bytes.includes(",")) {
+    return bytes.split(",").map(Number);
+  }
+
+  let buffer = Buffer.from(bytes, "base64");
+
+  let textBit = "";
+  buffer.forEach((byte) => {
+    textBit += byte.toString(2).padStart(8, "0");
+  });
+
+  let numbers = [];
+  for (let i = 0; i < textBit.length; i += 9) {
+    let nineBits = textBit.slice(i, i + 9);
+    if (nineBits.length === 9) {
+      numbers.push(parseInt(nineBits, 2));
+    }
+  }
+
+  return numbers;
+};
---
+++ tests/compression.test.js
@@ -0,0 +1,187 @@
+/*
+Примеры тестов: 
+простейшие короткие, 
+случайные - 50 чисел, 100 чисел, 500 чисел, 1000 чисел, 
+граничные - все числа 1 знака, 
+все числа из 2х знаков, 
+все числа из 3х знаков, 
+каждого числа по 3 - всего чисел 900. 
+
+*/
+
+import { expect } from "chai";
+import { serialize, deserialize } from "../index.js";
+
+function compressionRatio(original, serialized) {
+  const originalSize = JSON.stringify(original).length;
+  return (serialized.length / originalSize) * 100;
+}
+
+function generateRandomNumbers(count) {
+  return Array.from(
+    { length: count },
+    () => Math.floor(Math.random() * 300) + 1
+  );
+}
+
+describe("Compression tests: ", function () {
+  describe("simple short arrays", function () {
+    it("should correctly serialize and deserialize [0]", function () {
+      const numbers = [0];
+      const serialized = serialize(numbers);
+      const deserialized = deserialize(serialized);
+
+      expect(deserialized).to.deep.equal(numbers);
+      const ratio = compressionRatio(numbers, serialized);
+      expect(ratio).to.be.below(100);
+
+      console.log(`Исходная строка: ${numbers}`);
+      console.log(`Сжатая строка: ${serialized}`);
+      console.log(`Коэффициент сжатия: ${ratio.toFixed(2)}%`);
+    });
+
+    it("should correctly serialize and deserialize [1, 2, 3]", function () {
+      const numbers = [1, 2, 3];
+      const serialized = serialize(numbers);
+      const deserialized = deserialize(serialized);
+
+      expect(deserialized).to.deep.equal(numbers);
+      const ratio = compressionRatio(numbers, serialized);
+      expect(ratio).to.be.below(100);
+
+      console.log(`Исходная строка: ${JSON.stringify(numbers)}`);
+      console.log(`Сжатая строка: ${serialized}`);
+      console.log(`Коэффициент сжатия: ${ratio.toFixed(2)}%`);
+    });
+
+    it("should correctly serialize and deserialize array [300, 299, 298]", function () {
+        const numbers = [300, 299, 298];
+        const serialized = serialize(numbers);
+        const deserialized = deserialize(serialized);
+    
+        expect(deserialized).to.deep.equal(numbers);
+        const ratio = compressionRatio(numbers, serialized);
+        expect(ratio).to.be.below(100);
+    
+        console.log(`Исходная строка: ${JSON.stringify(numbers)}`);
+        console.log(`Сжатая строка: ${serialized}`);
+        console.log(`Коэффициент сжатия: ${ratio.toFixed(2)}%`);
+      });
+  });
+
+  describe("random arrays", function () {
+    it("should correctly serialize and deserialize random 50", function () {
+        const numbers = generateRandomNumbers(50);
+        const serialized = serialize(numbers);
+        const deserialized = deserialize(serialized);
+    
+        expect(deserialized).to.deep.equal(numbers);
+        const ratio = compressionRatio(numbers, serialized);
+        expect(ratio).to.be.below(100);
+        
+        console.log(`Исходная строка: ${JSON.stringify(numbers)}`);
+        console.log(`Сжатая строка: ${serialized}`);
+        console.log(`Коэффициент сжатия: ${ratio.toFixed(2)}%`);
+    });
+
+    it("should correctly serialize and deserialize random 100", function () {
+        const numbers = generateRandomNumbers(100);
+        const serialized = serialize(numbers);
+        const deserialized = deserialize(serialized);
+    
+        expect(deserialized).to.deep.equal(numbers);
+        const ratio = compressionRatio(numbers, serialized);
+        expect(ratio).to.be.below(100);
+        
+        console.log(`Исходная строка: ${JSON.stringify(numbers)}`);
+        console.log(`Сжатая строка: ${serialized}`);
+        console.log(`Коэффициент сжатия: ${ratio.toFixed(2)}%`);
+    });
+
+    it("should correctly serialize and deserialize random 500", function () {
+        const numbers = generateRandomNumbers(500);
+        const serialized = serialize(numbers);
+        const deserialized = deserialize(serialized);
+    
+        expect(deserialized).to.deep.equal(numbers);
+        const ratio = compressionRatio(numbers, serialized);
+        expect(ratio).to.be.below(100);
+        
+        console.log(`Исходная строка: ${JSON.stringify(numbers)}`);
+        console.log(`Сжатая строка: ${serialized}`);
+        console.log(`Коэффициент сжатия: ${ratio.toFixed(2)}%`);
+    });
+
+    it("should correctly serialize and deserialize random 1000", function () {
+        const numbers = generateRandomNumbers(500);
+        const serialized = serialize(numbers);
+        const deserialized = deserialize(serialized);
+    
+        expect(deserialized).to.deep.equal(numbers);
+        const ratio = compressionRatio(numbers, serialized);
+        expect(ratio).to.be.below(100);
+        
+        console.log(`Исходная строка: ${JSON.stringify(numbers)}`);
+        console.log(`Сжатая строка: ${serialized}`);
+        console.log(`Коэффициент сжатия: ${ratio.toFixed(2)}%`);
+    });
+  });
+
+  describe("edge case - number of digits", function () {
+    it("should correctly serialize and deserialize edge case with all 1s", function () {
+        const numbers = Array(1000).fill(1);
+        const serialized = serialize(numbers);
+        const deserialized = deserialize(serialized);
+    
+        expect(deserialized).to.deep.equal(numbers);
+        const ratio = compressionRatio(numbers, serialized);
+        expect(ratio).to.be.below(100);
+    
+        console.log(`Исходная строка: ${JSON.stringify(numbers)}`);
+        console.log(`Сжатая строка: ${serialized}`);
+        console.log(`Коэффициент сжатия: ${ratio.toFixed(2)}%`);
+      });
+    
+      it("should correctly serialize and deserialize edge case with all 2-digit numbers", function () {
+        const numbers = Array(1000).fill(99);
+        const serialized = serialize(numbers);
+        const deserialized = deserialize(serialized);
+    
+        expect(deserialized).to.deep.equal(numbers);
+        const ratio = compressionRatio(numbers, serialized);
+        expect(ratio).to.be.below(100);
+    
+        console.log(`Исходная строка: ${JSON.stringify(numbers)}`);
+        console.log(`Сжатая строка: ${serialized}`);
+        console.log(`Коэффициент сжатия: ${ratio.toFixed(2)}%`);
+      });
+    
+      it("should correctly serialize and deserialize edge case with all 3-digit numbers", function () {
+        const numbers = Array(1000).fill(299);
+        const serialized = serialize(numbers);
+        const deserialized = deserialize(serialized);
+    
+        expect(deserialized).to.deep.equal(numbers);
+        const ratio = compressionRatio(numbers, serialized);
+        expect(ratio).to.be.below(100);
+    
+        console.log(`Исходная строка: ${JSON.stringify(numbers)}`);
+        console.log(`Сжатая строка: ${serialized}`);
+        console.log(`Коэффициент сжатия: ${ratio.toFixed(2)}%`);
+      });
+
+      it("should correctly serialize and deserialize 900 numbers in the pattern [1, 10, 100]", function () {
+        const numbers = Array.from({ length: 300 }, () => [1, 10, 100]).flat();
+        const serialized = serialize(numbers);
+        const deserialized = deserialize(serialized);
+    
+        expect(deserialized).to.deep.equal(numbers);
+        const ratio = compressionRatio(numbers, serialized);
+        expect(ratio).to.be.below(100);
+    
+        console.log(`Исходная строка: ${JSON.stringify(numbers)}`);
+        console.log(`Сжатая строка: ${serialized}`);
+        console.log(`Коэффициент сжатия: ${ratio.toFixed(2)}%`);
+      });
+  })
+});
